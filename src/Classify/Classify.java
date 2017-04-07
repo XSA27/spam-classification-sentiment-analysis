6c16bb39b01c6bb6b758d5894ae0144d624138e0
Mikel Abad <MikelAbadG@gmail.com>
2017-04-07 20:06:17 +0200

    Clase para clasificar el test

--- src/Classify/Classify.java
+++ src/Classify/Classify.java
@@ -1,5 +1,39 @@
 package Classify;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+import arff2bow.Lectura;
+import weka.classifiers.Classifier;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+
 public class Classify {
-	//TODO
+	
+	public static void main(String[] args) throws Exception {
+		String testPath = args[0];
+		String modelPath = args[1];
+		String exportPath = args[2];
+		
+		Lectura lec = new Lectura();
+		Instances dataTest = lec.cargarDatos(testPath);
+		Classifier classifier = (Classifier) SerializationHelper.read(modelPath);
+		
+		File fout = new File(exportPath);
+		FileOutputStream fos = new FileOutputStream(fout);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));	
+		
+		bw.write("Predictions for the test: ");
+		bw.newLine();
+		int i = 0;
+		for (Instance c : dataTest){
+			bw.write("  Instance " + i + ":  " + ((Double) (classifier.classifyInstance(c))).toString());
+			i++;
+			bw.newLine();
+		}
+		bw.close();
+	}
 }
