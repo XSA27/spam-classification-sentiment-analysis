0c9681ae435328cac43bbeea28df254cf09ad9f3
XSA27 <xsantibanez001@ehu.eus>
2017-04-07 09:57:32 +0200

    arreglo matriz de confusión

--- src/baseline/Results.java
+++ src/baseline/Results.java
@@ -5,18 +5,10 @@ import weka.classifiers.Evaluation;
 public class Results {
 
 	public void imprimirResultados(Evaluation evaluator) throws Exception {
-		double confMatrix[][] = evaluator.confusionMatrix();
 
 		System.out.println(evaluator.toSummaryString());
 		System.out.println(evaluator.toClassDetailsString());
-
-		for (int row_i = 0; row_i < confMatrix.length; row_i++) {
-			for (int col_i = 0; col_i < confMatrix.length; col_i++) {
-				System.out.print(confMatrix[row_i][col_i]);
-				System.out.print("|");
-			}
-			System.out.println();
-		}
+		System.out.println(evaluator.toMatrixString());
 
 	}
 }
